0e7664742985398b5790dbc2a7b531cbecb47b9b
Emilija Abromaitytė <emilija.abromaityte@mif.stud.vu.lt>
2026-02-22 16:49:44 +0200

    standartizuoti grafikai

--- lab1.py
+++ lab1.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from pandas.plotting import scatter_matrix
 import matplotlib.dates as mdates
 import seaborn as sns
+import matplotlib.dates as mdates
 
 
 data = pd.read_csv('INV12.csv')
@@ -79,10 +80,10 @@ plt.show()
 
 # min max
 numeric_cols = data.select_dtypes(include=['number']).columns
-normalized_minmax = (data[numeric_cols] - data[numeric_cols].min()) / (data[numeric_cols].max() - data[numeric_cols].min())
+normalized_minmax = (data_hourly[numeric_cols] - data_hourly[numeric_cols].min()) / (data_hourly[numeric_cols].max() - data_hourly[numeric_cols].min())
 # %% 
 #standartizacija
-standartizuotas = (data[numeric_cols] - data[numeric_cols].mean()) / data[numeric_cols].std()
+standartizuotas = (data_hourly[numeric_cols] - data_hourly[numeric_cols].mean()) / data_hourly[numeric_cols].std()
 # %%
 colors = ["#D00000", "#ffba08", "#cbff8c", "#8fe388", "#1b998b", "#3185fc",
           "#5d2e8c", "#46237a", "#ff7b9c", "#ff9b85"]
@@ -151,20 +152,20 @@ plt.show()
 
 #%%
 #laikute skirtinguose plot'uose
-fig, axes = plt.subplots(5, 2, figsize=(20, 16),sharey=True)
-axes = axes.flatten()
+# fig, axes = plt.subplots(5, 2, figsize=(20, 16),sharey=True)
+# axes = axes.flatten()
 
-for i, col in enumerate(columns):
-    axes[i].plot(data_hourly.index, data_hourly[col], 
-                 color=colors[i], linewidth=2)
-    axes[i].set_title(f"Grandinė {i+1}")
-    axes[i].xaxis.set_major_locator(mdates.DayLocator(interval=10))
-    axes[i].xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
-    axes[i].grid(alpha=0.3)
+# for i, col in enumerate(columns):
+#     axes[i].plot(data_hourly.index, data_hourly[col], 
+#                  color=colors[i], linewidth=2)
+#     axes[i].set_title(f"Grandinė {i+1}")
+#     axes[i].xaxis.set_major_locator(mdates.DayLocator(interval=10))
+#     axes[i].xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
+#     axes[i].grid(alpha=0.3)
 
-plt.tight_layout()
-plt.savefig("grafikai/laikute_subplots.png")
-plt.show()
+# plt.tight_layout()
+# plt.savefig("grafikai/laikute_subplots.png")
+# plt.show()
 # %%
 plt.figure(figsize=(20, 8))
 sns.heatmap(data_hourly[columns].T, cmap='YlOrRd', cbar_kws={'label': 'kWh'})
@@ -182,3 +183,83 @@ plt.show()
 print(tick_positions)
 ## DG: As manau sitas geras irgi bet cia demokratija, tai subalsuokime
 # %%
+#Standartizuotos aibes histogramos
+
+fig, axes = plt.subplots(2, 5, figsize=(20, 8), sharey=True)
+axes = axes.flatten()
+columns = [f"string_{i}" for i in range(1, 11)]
+
+for i, col in enumerate(columns):
+    axes[i].hist(standartizuotas[col], bins=15, color = colors[i], 
+                 alpha = 0.7, edgecolor=colors[i])
+    axes[i].set_title(col)
+    axes[i-1].set_title(f"Grandinė {i}")
+
+fig.supxlabel("Elektra, pagaminta grandinėje (kWh)")
+fig.supylabel("Dažnis")
+fig.suptitle("Elektros, pagamintos grandinėse per valandą, histograma (standartizuotos reikšmės)", fontsize=18)
+
+plt.tight_layout()
+plt.savefig("grafikai/histogramos_standartizuotos.png")
+plt.show()
+
+# %%
+#Staciakampes diagramos standartizuotos
+columns = [f"string_{i}" for i in range(1, 11)]
+data = [standartizuotas[col] for col in columns]
+
+plt.figure(figsize=(12, 6))
+box = plt.boxplot(data, patch_artist=True)
+
+for patch, c in zip(box["boxes"], colors):
+    patch.set_facecolor(c)
+    patch.set_alpha(0.7)
+
+plt.xticks(
+    range(1, 11),
+    [f"Grandinė {i}" for i in range(1, 11)]
+)
+
+plt.ylabel("Elektra, pagaminta grandinėje (kWh)")
+plt.title("Per valandą pagaminta elektra skirtingose grandinėse (standartizuotos reikšmės)") 
+
+plt.grid(axis="y", alpha=0.3)
+plt.savefig("grafikai/boxplotai_standartizuoti.png")
+plt.show()
+
+#%%
+#heatmapas laikutei standartizuotos reikšmės
+
+plt.figure(figsize=(20, 8))
+sns.heatmap(standartizuotas[columns].T, cmap='YlOrRd', cbar_kws={'label': 'kWh'})
+ax = plt.gca()
+new_labels = [f"Grandinė {i}" for i in range(1, 11)]
+ax.set_yticklabels(new_labels, rotation=0)
+tick_positions = range(0, len(standartizuotas.index), 16)
+ax.set_xticks(tick_positions)
+ax.set_xticklabels([standartizuotas.index[i].strftime('%m-%d') for i in tick_positions])
+plt.xlabel("Data")
+plt.title("Pagamintos elektros grandinėse šilumos žemėlapis (standartizuotos reikšmės)")
+plt.tight_layout()
+plt.savefig("grafikai/laikute_heatmap_standartizuotas.png")
+plt.show()
+print(tick_positions)
+
+# %%
+
+columns = [f"string_{i}" for i in range(1, 11)]
+display_names = [f"Grandinė {i}" for i in range(1, 11)]
+
+temp_df = standartizuotas[columns].copy()
+temp_df.columns = display_names  
+
+sm = scatter_matrix(
+    temp_df,
+    alpha=0.6,
+    figsize=(20, 20),
+    diagonal='kde',
+    color=colors[5]  
+)
+
+plt.suptitle("Grandinėse pagamintos elektros sklaidos diagrama (standartizuotos reikšmės)", fontsize=24)
+plt.savefig("grafikai/sklaidos_standartizuoti.png")
