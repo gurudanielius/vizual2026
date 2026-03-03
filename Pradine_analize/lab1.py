9bdb53f89776a9f39af02fae6657a8ea288a9e61
gurudanielius <gurvic.danielius@gmail.com>
2026-03-04 01:27:08 +0200

    folderis yay

---
+++ Pradine_analize/lab1.py
@@ -0,0 +1,282 @@
+#%%
+import pandas as pd
+import matplotlib.pyplot as plt
+from pandas.plotting import scatter_matrix
+import seaborn as sns
+import matplotlib.dates as mdates
+
+data = pd.read_csv('INV12.csv')
+data.describe()
+# Praleistų stebėjimų nėra.
+data['Timestamp']= pd.to_datetime(data['Timestamp']) #Pakeiciam i datetime agregavimo logikai veliau
+data["month_day_hour"]=data['Timestamp'].dt.strftime('%m-%d-%H')
+
+data.groupby(data["month_day_hour"]).sum(numeric_only=True)
+data.columns = ['Timestamp'] + [f'string_{i}' for i in range(1, 11)] + ['month_day_hour']
+
+## NUSPRENDEME NA REIKSMES TIESIOG PASALINTI
+data=data.dropna(subset=[f'string_{i}' for i in range(1, 11)])
+#%%
+
+filtered_data = data[data['Timestamp'].dt.hour.between(10, 17)]
+data_hourly=filtered_data.groupby(filtered_data["month_day_hour"]).sum(numeric_only=True)
+data_hourly
+
+#%%
+#Aprašomoji statistika 
+data_hourly.describe() 
+
+#%%
+#Taškai atsiskyrėliai
+numeric = data_hourly.select_dtypes(include="number")
+
+outliers = {}
+
+for col in numeric.columns:
+    Q1 = numeric[col].quantile(0.25)
+    Q3 = numeric[col].quantile(0.75)
+    IQR = Q3 - Q1
+
+    lower = Q1 - 1.5 * IQR
+    upper = Q3 + 1.5 * IQR
+
+    mask = (numeric[col] < lower) | (numeric[col] > upper)
+    outliers[col] = numeric.loc[mask, col]
+
+for col, vals in outliers.items():
+
+    print(f"{col}: {len(vals)} outliers")
+
+#%%
+#Skaičiuojamos koreliacijos
+correlation_matrix = data_hourly.select_dtypes(include=['number']).corr()
+print(correlation_matrix)
+
+#%%
+correlation_matrix.index = [f"Grandinė {i+1}" for i in range(len(correlation_matrix.index))]
+correlation_matrix.columns = [f"Grandinė {i+1}" for i in range(len(correlation_matrix.columns))]
+
+plt.figure(figsize=(12, 10))
+sns.heatmap(
+    correlation_matrix,
+    annot=True,
+    fmt='.2f',
+    cmap='coolwarm',
+    center=0,
+    square=True,
+    linewidths=1,
+    cbar_kws={"shrink": 0.8}
+)
+
+plt.title("Koreliacijų šilumos žemėlapis", fontsize=24)
+plt.tight_layout()
+plt.savefig("grafikai/koreliacijos_heatmap.png")
+plt.show()
+#%%
+
+# min max
+numeric_cols = data.select_dtypes(include=['number']).columns
+normalized_minmax = (data_hourly[numeric_cols] - data_hourly[numeric_cols].min()) / (data_hourly[numeric_cols].max() - data_hourly[numeric_cols].min())
+#%%
+#standartizacija
+standartizuotas = (data_hourly[numeric_cols] - data_hourly[numeric_cols].mean()) / data_hourly[numeric_cols].std()
+#%%
+colors = ["#D00000", "#ffba08", "#cbff8c", "#8fe388", "#1b998b", "#3185fc",
+          "#5d2e8c", "#46237a", "#ff7b9c", "#ff9b85"]
+
+columns = [f"string_{i}" for i in range(1, 11)]
+display_names = [f"Grandinė {i}" for i in range(1, 11)]
+
+temp_df = data_hourly[columns].copy()
+temp_df.columns = display_names  
+
+sm = scatter_matrix(
+    temp_df,
+    alpha=0.6,
+    figsize=(20, 20),
+    diagonal='kde',
+    color=colors[5]  
+)
+plt.suptitle(
+    "Grandinėse pagamintos elektros energijos sklaidos matrica",
+    fontsize=30)
+plt.tight_layout(rect=[0, 0, 1, 0.97])
+plt.savefig("grafikai/sklaidos.png")
+
+#%%
+# Dažnio diagramos - histogramos
+fig, axes = plt.subplots(2, 5, figsize=(20, 8), sharey=True, sharex=True)
+axes = axes.flatten()
+columns = [f"string_{i}" for i in range(1, 11)]
+
+for i, col in enumerate(columns):
+    axes[i].hist(data_hourly[col], bins=15, color = colors[i], 
+                 alpha = 0.7, edgecolor=colors[i])
+    axes[i].set_title(col)
+    axes[i].set_title(f"Grandinė {i+1}")
+
+fig.supxlabel("Pagaminta elektros energija (kWh)")
+fig.supylabel("Dažnis")
+fig.suptitle("Pagaminamos elektros energijos pasiskirstymas tarp grandinių", fontsize=26)
+
+plt.savefig("grafikai/histogramos.png")
+plt.show()
+
+#%%
+# Stačiakampės diagramos
+columns = [f"string_{i}" for i in range(1, 11)]
+data = [data_hourly[col] for col in columns]
+
+plt.figure(figsize=(12, 6))
+box = plt.boxplot(data, patch_artist=True)
+
+for patch, c in zip(box["boxes"], colors):
+    patch.set_facecolor(c)
+    patch.set_alpha(0.7)
+
+plt.xticks(
+    range(1, 11),
+    [f"Grandinė {i}" for i in range(1, 11)]
+)
+
+plt.ylabel("Pagaminta elektros energija (kWh)")
+plt.title("Pagamintos elektros energijos palyginimas tarp grandinių") 
+
+plt.grid(axis="y", alpha=0.3)
+plt.gca().spines['top'].set_visible(False)
+plt.gca().spines['right'].set_visible(False)
+plt.gca().spines['left'].set_visible(False)
+plt.gca().spines['bottom'].set_visible(False)
+plt.savefig("grafikai/boxplotai.png")
+plt.show()
+
+#%%
+#laikute skirtinguose plot'uose
+# fig, axes = plt.subplots(5, 2, figsize=(20, 16),sharey=True)
+# axes = axes.flatten()
+
+# for i, col in enumerate(columns):
+#     axes[i].plot(data_hourly.index, data_hourly[col], 
+#                  color=colors[i], linewidth=2)
+#     axes[i].set_title(f"Grandinė {i+1}")
+#     axes[i].xaxis.set_major_locator(mdates.DayLocator(interval=10))
+#     axes[i].xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
+#     axes[i].grid(alpha=0.3)
+
+# plt.tight_layout()
+# plt.savefig("grafikai/laikute_subplots.png")
+# plt.show()
+#%%
+plt.figure(figsize=(20, 8))
+sns.heatmap(data_hourly[columns].T, cmap='YlOrRd', cbar_kws={'label': 'kWh'})
+ax = plt.gca()
+new_labels = [f"Grandinė {i}" for i in range(1, 11)]
+ax.set_yticklabels(new_labels, rotation=0)
+tick_positions = range(0, len(data_hourly.index), 16)
+ax.set_xticks(tick_positions)
+ax.set_xticklabels([data_hourly.index[i][:5] for i in tick_positions])
+plt.xlabel("Data")
+plt.title("Pagamintos elektros energijos dinamika", fontsize=24)
+plt.tight_layout()
+plt.savefig("grafikai/laikute_heatmap.png")
+plt.show()
+print(tick_positions)
+## DG: As manau sitas geras irgi bet cia demokratija, tai subalsuokime
+#%%
+#Standartizuotos aibes histogramos
+
+fig, axes = plt.subplots(2, 5, figsize=(20, 8), sharey=True, sharex=True)
+axes = axes.flatten()
+columns = [f"string_{i}" for i in range(1, 11)]
+
+for i, col in enumerate(columns):
+    axes[i].hist(standartizuotas[col], bins=15, color = colors[i], 
+                 alpha = 0.7, edgecolor=colors[i])
+    axes[i].set_title(col)
+    axes[i].set_title(f"Grandinė {i+1}")
+
+fig.supxlabel("Standartizuoti vienetai")
+fig.supylabel("Dažnis")
+fig.suptitle("Pagaminamos elektros energijos pasiskirstymas tarp grandinių (standartizuotos reikšmės)", fontsize=26)
+
+plt.tight_layout()
+plt.savefig("grafikai/histogramos_standartizuotos.png")
+plt.show()
+
+#%%
+#Staciakampes diagramos standartizuotos
+columns = [f"string_{i}" for i in range(1, 11)]
+data = [standartizuotas[col] for col in columns]
+
+plt.figure(figsize=(12, 6))
+box = plt.boxplot(data, patch_artist=True)
+
+for patch, c in zip(box["boxes"], colors):
+    patch.set_facecolor(c)
+    patch.set_alpha(0.7)
+
+plt.xticks(
+    range(1, 11),
+    [f"Grandinė {i}" for i in range(1, 11)]
+)
+
+plt.ylabel("Standartizuoti vienetai")
+plt.title("Pagamintos elektros energijos palyginimas tarp grandinių (standartizuotos reikšmės)", fontsize=15) 
+
+plt.grid(axis="y", alpha=0.3)
+plt.gca().spines['top'].set_visible(False)
+plt.gca().spines['right'].set_visible(False)
+plt.gca().spines['left'].set_visible(False)
+plt.gca().spines['bottom'].set_visible(False)
+plt.savefig("grafikai/boxplotai_standartizuoti.png")
+plt.show()
+
+#%%
+#heatmapas laikutei standartizuotos reikšmės
+
+plt.figure(figsize=(20, 8))
+sns.heatmap(standartizuotas[columns].T, cmap='YlOrRd', cbar_kws={'label': 'Standartizuoti vienetai'})
+ax = plt.gca()
+new_labels = [f"Grandinė {i}" for i in range(1, 11)]
+ax.set_yticklabels(new_labels, rotation=0)
+tick_positions = range(0, len(standartizuotas.index), 16)
+ax.set_xticks(tick_positions)
+ax.set_xticklabels([standartizuotas.index[i][:5] for i in tick_positions])
+plt.xlabel("Data")
+plt.title("Pagamintos elektros energijos dinamika (standartizuotos reikšmės)",fontsize=24)
+plt.tight_layout()
+plt.savefig("grafikai/laikute_heatmap_standartizuotas.png")
+plt.show()
+print(tick_positions)
+
+
+# %%
+
+columns = [f"string_{i}" for i in range(1, 11)]
+display_names = [f"Grandinė {i}" for i in range(1, 11)]
+
+temp_df = standartizuotas[columns].copy()
+temp_df.columns = display_names  
+
+sm = scatter_matrix(
+    temp_df,
+    alpha=0.6,
+    figsize=(20, 20),
+    diagonal='kde',
+    color=colors[5]
+)
+
+plt.suptitle(
+    "Grandinėse pagamintos elektros energijos sklaidos matrica (standartizuotos reikšmės)",
+    fontsize=30)
+plt.tight_layout(rect=[0, 0, 1, 0.97])
+
+plt.savefig("grafikai/sklaidos_standartizuoti.png")
+# %%
+data_agg=data_hourly.copy()
+data_agg["month"]= data_hourly.index.str[0:2].astype(str)
+data_agg=data_agg.groupby("month").sum(numeric_only=True)
+data_agg["total"]=data_agg.sum(axis=1) 
+data_agg=data_agg.drop(columns=[f"string_{i}" for i in range(1, 11)])
+data_agg.plot(kind="bar", y="total", legend=False, color=colors[5], alpha=0.7)
+plt.xlabel("Mėnuo")
