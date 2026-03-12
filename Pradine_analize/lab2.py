87ba4b3ef909fb7895e13ca32d00af5df3807ba7
Benas73 <benas.manionis@gmail.com>
2026-03-12 12:41:57 +0200

    lab2_1

--- Pradine_analize/lab2.py
+++ Pradine_analize/lab2.py
@@ -8,6 +8,7 @@ from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE, MDS
 from sklearn.preprocessing import StandardScaler
 import umap
+import numpy as np
 
 data = pd.read_csv('INV12.csv')
 data.describe()
@@ -20,10 +21,7 @@ data.columns = ['Timestamp'] + [f'string_{i}' for i in range(1, 11)] + ['month_d
 
 ## NUSPRENDEME NA REIKSMES TIESIOG PASALINTI
 data=data.dropna(subset=[f'string_{i}' for i in range(1, 11)])
-#%%
 filtered_data = data[data['Timestamp'].dt.hour.between(10, 17)]
-data_hourly=filtered_data.groupby(filtered_data["month_day_hour"]).sum(numeric_only=True)
-data_hourly
 # %%
 #Standartizavimas
 X = filtered_data.drop(columns=['Timestamp', 'month_day_hour'])
@@ -36,18 +34,23 @@ X_pca = pca.fit_transform(X_scaled)
 
 plt.figure(figsize=(8, 6))
 plt.scatter(X_pca[:, 0], X_pca[:, 1], s=8)
-plt.title("PCA (2 komponenčių) projekcija")
+plt.title("PCA projekcija")
 plt.axis('equal')
 plt.show()
 
 #%%
 #t-SNE
-tsne = TSNE(n_components=2, perplexity=30, random_state=1)
+# Color points by one of the original features (columns) in X_scaled.
+# Change `color_col` to 0..9 to color by a different source column.
+color_col = 0
+
+tsne = TSNE(n_components=2, random_state=1)
 X_2d = tsne.fit_transform(X_scaled)
 min_val = min(X_2d[:, 0].min(), X_2d[:, 1].min())
 max_val = max(X_2d[:, 0].max(), X_2d[:, 1].max())
 plt.figure(figsize=(8, 6))
-plt.scatter(X_2d[:, 0], X_2d[:, 1], s=8)
+plt.scatter(X_2d[:, 0], X_2d[:, 1], s=8, c=X_scaled[:, color_col], cmap='viridis')
+plt.colorbar(label=f"Feature {color_col} value")
 plt.title("t-SNE projekcija")
 plt.xlim(min_val, max_val)
 plt.ylim(min_val, max_val)
@@ -66,9 +69,7 @@ plt.show()
 
 #%%
 #UMAP
-# Note: setting random_state forces UMAP to run single-threaded (n_jobs=1).
-# If you want parallelism, remove random_state (but results will not be deterministic).
-reducer = umap.UMAP(n_neighbors=15, min_dist=0.1, metric="euclidean", random_state=1, n_jobs=1)
+reducer = umap.UMAP(n_neighbors=15, min_dist=0.1, metric="euclidean", random_state=1)
 X_umap = reducer.fit_transform(X_scaled)
 
 plt.figure(figsize=(8, 6))
