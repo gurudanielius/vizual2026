d65e1a0e23a42ea762c93b2ebbefb5e87b21d433
gurudanielius <gurvic.danielius@gmail.com>
2026-03-17 22:37:50 +0200

    yo grafique de mds

--- Pradine_analize/lab2.py
+++ Pradine_analize/lab2.py
@@ -9,6 +9,85 @@ import umap
 import numpy as np
 
 
+
+# %%
+def plot_tsne_panel(tsne_results, hyperparameter, title, labels, ncols=3, figsize=(14, 8)):
+    import numpy as np
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+
+    n_plots = len(tsne_results)
+    nrows = (n_plots + ncols - 1) // ncols
+
+    labels = np.array(labels) 
+    unique_labels = sorted(labels, key=lambda x: int(x.split('_')[1]))
+    unique_labels = list(dict.fromkeys(unique_labels))
+    label_names = unique_labels
+
+    palette = sns.color_palette("husl", len(unique_labels))
+    color_map = dict(zip(label_names, palette))
+
+    fig, axes = plt.subplots(nrows, ncols, figsize=figsize, constrained_layout=True)
+    axes = np.atleast_1d(axes).flatten()
+
+    for i, (value, X_2d) in enumerate(tsne_results.items()):
+        ax = axes[i]
+
+        df = pd.DataFrame({
+            "Dim1": X_2d[:, 0],
+            "Dim2": X_2d[:, 1],
+            "string": labels  
+        })
+
+        sns.scatterplot(
+            data=df,
+            x="Dim1",
+            y="Dim2",
+            hue="string",
+            palette=color_map,
+            hue_order=label_names,
+            s=30,
+            legend=False,
+            ax=ax
+        )
+
+        ax.set_title(f"{hyperparameter} = {value}")
+        ax.set_box_aspect(1)
+
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_xlabel("")
+        ax.set_ylabel("")
+
+    for j in range(i + 1, len(axes)):
+        fig.delaxes(axes[j])
+
+    handles = [
+        plt.Line2D(
+            [0], [0],
+            marker='o',
+            color='w',
+            markerfacecolor=color_map[name],
+            markersize=6,
+            linestyle=''
+        )
+        for name in label_names
+    ]
+
+    fig.legend(
+        handles,
+        label_names,
+        title="Grandinės",
+        loc="center left",
+        bbox_to_anchor=(1.02, 0.5)
+    )
+
+    plt.suptitle(title)
+    # plt.tight_layout(rect=[0, 0, 0.9, 1]) 
+    plt.savefig(f"{title.replace(' ', '_')}_{hyperparameter.replace(' ', '_')}.png", dpi=300, bbox_inches='tight')
+    plt.show()
+
 # %%
 data = pd.read_csv('INV12.csv')
 data.columns = ['Timestamp'] + [f'string_{i}' for i in range(1, 11)] 
@@ -21,10 +100,12 @@ data["category"]=data["month_day"].map(mapping["category"])
 
 
 
+
 # %%
 string_cols = [f'string_{i}' for i in range(1, 11)]
 data_minmax_by_category = data.groupby('category')[string_cols].transform(lambda x: (x - x.min()) / (x.max() - x.min()))
 data[string_cols] = data_minmax_by_category
+
 
 
 
@@ -56,14 +137,17 @@ result=result.sort_values(by=['month_day', 'string']).reset_index(drop=True)
 result.index.name = 'id'
 
 
+
 # %%
 data_high = result[result['category'] == 'High'].drop(columns=['category', 'month_day'])
 data_medium = result[result['category'] == 'Medium'].drop(columns=['category', 'month_day'])
 data_low = result[result['category'] == 'Low'].drop(columns=['category', 'month_day'])
 
 
+
 # %%
 X_high, X_medium, X_low = data_high.drop(columns=['string']).fillna(0), data_medium.drop(columns=['string']).fillna(0), data_low.drop(columns=['string']).fillna(0)
+
 
 
 
@@ -82,6 +166,7 @@ tsne_high_result = tsne_high.fit_transform(X_high)
 
 
 
+
 # %%
 X_pca_df = pd.DataFrame(tsne_high_result, columns=['PC1', 'PC2'])
 X_pca_df['string'] = data_high['string'].values
@@ -93,6 +178,7 @@ plt.xlabel('PC1')
 plt.ylabel('PC2')
 plt.axis('equal')
 plt.show()
+
 
 # %%
 tsne_high = TSNE(
@@ -106,6 +192,7 @@ tsne_high_result = tsne_high.fit_transform(X_high)
 
 
 
+
 # %%
 X_pca_df = pd.DataFrame(tsne_high_result, columns=['PC1', 'PC2'])
 X_pca_df['string'] = data_high['string'].values
@@ -117,6 +204,7 @@ plt.xlabel('PC1')
 plt.ylabel('PC2')
 plt.axis('equal')
 plt.show()
+
 
 # %%
 #TSNE medium
@@ -140,6 +228,7 @@ plt.ylabel('PC2')
 plt.axis('equal')
 plt.show()
 
+
 # %%
 #TSNE low
 
@@ -162,6 +251,7 @@ plt.ylabel('PC2')
 plt.axis('equal')
 plt.show()
 
+
 # %%
 #UMAP high
 umap_high = umap.UMAP(
@@ -182,11 +272,12 @@ plt.legend(title='Grandinės', bbox_to_anchor=(1.05, 1), loc='upper left')
 plt.title('UMAP Projekcija (High)')
 plt.xlabel('UMAP1')
 plt.ylabel('UMAP2')
-plt.axis('equal')
 plt.show()
+
 
 # %%
 #UMAP medium
+
 
 
 
@@ -196,73 +287,240 @@ plt.show()
 
 
 
+
+# %%
+niter_values = [50, 100, 250, 500, 750, 1000]
+fig, axes = plt.subplots(2, 3, figsize=(18, 12))
+axes = axes.ravel()
+
+results = []
+handles, labels = None, None
+
+for i, niter in enumerate(niter_values):
+    mds = MDS(
+        n_components=2,
+        n_init=1,
+        random_state=80085,
+        init='random',
+        metric=True,
+        dissimilarity='euclidean',
+        max_iter=niter,
+        n_jobs=-1,
+        eps=1e-10,
+        normalized_stress=True
+    )
+
+    mds_result = mds.fit_transform(X_high)
+
+    X_mds_df = pd.DataFrame(mds_result, columns=['MDS1', 'MDS2'])
+    X_mds_df['string'] = data_high['string'].values
+
+    ax = axes[i]
+    lims=[min(np.floor(X_mds_df['MDS1'].min()), np.floor(X_mds_df['MDS2'].min())),max(np.ceil(X_mds_df['MDS1'].max()), np.ceil(X_mds_df['MDS2'].max()))]
+    ax.set_xlim(lims)
+    ax.set_ylim(lims)
+    ax.set_aspect('equal', adjustable='box')
+    sns.scatterplot(
+        data=X_mds_df,
+        x='MDS1',
+        y='MDS2',
+        hue='string',
+        palette='husl',
+        s=75,
+        hue_order=[f'string_{j}' for j in range(1, 11)],
+        ax=ax,
+        legend=(i == 0),
+        
+    )
+
+    ax.set_title(f'MDS Projekcija (max_iter={niter}). Stress: {mds.stress_:.4f}')
+    ax.set_xlabel('MDS1')
+    ax.set_ylabel('MDS2')
+
+    if i == 0:
+        handles, labels = ax.get_legend_handles_labels()
+        ax.legend_.remove()
+
+    results.append({
+        'max_iter': niter,
+        'stress': mds.stress_,
+        'used_iter': mds.n_iter_
+    })
+
+fig.legend(handles, labels, title='Grandinės',loc="right")
+fig.suptitle('MDS Projekcija. max_iter skirtingos reikšmės, n_init=1, init=random', fontsize=20)
+plt.show()
+
+pd.DataFrame(results)
+
+# %%
+
+
+# %%
+n_init_values = [1,2,4,8,16,20] 
+fig, axes = plt.subplots(2, 3, figsize=(18, 12))
+axes = axes.ravel()
+
+results = []
+handles, labels = None, None
+
+for i, n_init in enumerate(n_init_values):
+    mds = MDS(
+        n_components=2,
+        n_init=n_init,
+        random_state=80085,
+        init='random',
+        metric=True,
+        dissimilarity='euclidean',
+        max_iter=100,
+        n_jobs=-1,
+        eps=1e-10,
+        normalized_stress=True
+    )
+
+    mds_result = mds.fit_transform(X_high)
+
+    X_mds_df = pd.DataFrame(mds_result, columns=['MDS1', 'MDS2'])
+    X_mds_df['string'] = data_high['string'].values
+
+    ax = axes[i]
+    lims=[min(np.floor(X_mds_df['MDS1'].min()), np.floor(X_mds_df['MDS2'].min())),max(np.ceil(X_mds_df['MDS1'].max()), np.ceil(X_mds_df['MDS2'].max()))]
+    ax.set_xlim(lims)
+    ax.set_ylim(lims)
+    ax.set_aspect('equal', adjustable='box')
+    ax.set_title(f'MDS Projekcija (n_init={n_init}). Stress: {mds.stress_:.6f}')
+    ax.set_xlabel('MDS1')
+    ax.set_ylabel('MDS2')
+    sns.scatterplot(
+        data=X_mds_df,
+        x='MDS1',
+        y='MDS2',
+        hue='string',
+        palette='husl',
+        s=75,
+        hue_order=[f'string_{j}' for j in range(1, 11)],
+        ax=ax,
+        
+        legend=(i == 0),
+        
+    )
+	
+
+    if i == 0:
+        handles, labels = ax.get_legend_handles_labels()
+        ax.legend_.remove()
+
+    results.append({
+        'n_init': n_init,
+        'stress': mds.stress_,
+        'used_iter': mds.n_iter_
+    })
+
+fig.legend(handles, labels, title='Grandinės',loc="right")
+#set title
+fig.suptitle('MDS Projekcija. n_init skirtingos reikšmės, max_iter=100, init=random', fontsize=20)
+plt.show()
+
+pd.DataFrame(results)
+
 # %%
 #MDS high
 from sklearn.manifold import MDS
 mds_high = MDS(
 	n_components=2,
-    n_init=20,
+    n_init=16,
     random_state=80085,
     init='classical_mds',
 	metric='euclidean',
-	max_iter=4000,
-    n_jobs=-1)
+	max_iter=500,
+    n_jobs=-1,
+    normalized_stress=True)
 mds_high_result = mds_high.fit_transform(X_high)
 X_mds_df = pd.DataFrame(mds_high_result, columns=['MDS1', 'MDS2'])
 X_mds_df['string'] = data_high['string'].values
-plt.figure(figsize=(10, 6))
+plt.figure(figsize=(10, 8))
 sns.scatterplot(data=X_mds_df, x='MDS1', y='MDS2', hue='string', palette='husl', s=75, hue_order=[f'string_{i}' for i in range(1, 11)])
-plt.legend(title='Grandinės', bbox_to_anchor=(1.05, 1), loc='upper left')
-plt.title('MDS Projekcija')
+ax = plt.gca()
+lims = [
+    min(-3, -3),
+    max(3, 3)
+]
+ax.set_xlim(lims)
+ax.set_ylim(lims)
+ax.set_aspect('equal', adjustable='box')
+plt.legend(title='Grandinės', bbox_to_anchor=(1.05, 0.25), loc='lower left')
+plt.title(f'MDS Projekcija. Stress: {mds_high.stress_:.4f}, max_iter=500, n_init=16, init=classical_mds')
 plt.xlabel('MDS1')
 plt.ylabel('MDS2')
-plt.axis('equal')
 plt.show()
+
 
 # %%
 #MDS medium
 from sklearn.manifold import MDS
 mds_medium = MDS(
 	n_components=2,
-    n_init=20,
+    n_init=16,
     random_state=80085,
     init='classical_mds',
 	metric='euclidean',
-	max_iter=4000,
-    n_jobs=-1)
+	max_iter=500,
+    n_jobs=-1,
+    normalized_stress=True)
 mds_medium_result = mds_medium.fit_transform(X_medium)
 X_mds_df = pd.DataFrame(mds_medium_result, columns=['MDS1', 'MDS2'])
 X_mds_df['string'] = data_medium['string'].values
-plt.figure(figsize=(10, 6))
+plt.figure(figsize=(10, 8))
 sns.scatterplot(data=X_mds_df, x='MDS1', y='MDS2', hue='string', palette='husl', s=75, hue_order=[f'string_{i}' for i in range(1, 11)])
-plt.legend(title='Grandinės', bbox_to_anchor=(1.05, 1), loc='upper left')
-plt.title('MDS Projekcija')
+ax = plt.gca()
+lims = [
+    min(ax.get_xlim()[0], ax.get_ylim()[0]),
+    max(ax.get_xlim()[1], ax.get_ylim()[1])
+]
+ax.set_xlim(lims)
+ax.set_ylim(lims)
+ax.set_aspect('equal', adjustable='box')
+plt.legend(title='Grandinės', bbox_to_anchor=(1.05, 0.25), loc='lower left')
+plt.title(f'MDS Projekcija. Stress: {mds_medium.stress_:.4f}, max_iter=500, n_init=16, init=classical_mds')
 plt.xlabel('MDS1')
 plt.ylabel('MDS2')
-plt.axis('equal')
 plt.show()
+
 
 # %%
 #MDS low
 from sklearn.manifold import MDS
 mds_low = MDS(
 	n_components=2,
-	n_init=20,
+	n_init=16,
 	random_state=80085,
 	init='classical_mds',
 	metric='euclidean',
-	max_iter=4000,
-	n_jobs=-1)
+	max_iter=500,
+	n_jobs=-1,
+    normalized_stress=True)
 mds_low_result = mds_low.fit_transform(X_low)
 X_mds_df = pd.DataFrame(mds_low_result, columns=['MDS1', 'MDS2'])
 X_mds_df['string'] = data_low['string'].values
-plt.figure(figsize=(10, 6))
+plt.figure(figsize=(10,8))
 sns.scatterplot(data=X_mds_df, x='MDS1', y='MDS2', hue='string', palette='husl', s=75, hue_order=[f'string_{i}' for i in range(1, 11)])
-plt.legend(title='Grandinės', bbox_to_anchor=(1.05, 1), loc='upper left')
-plt.title('MDS Projekcija')
+ax = plt.gca()
+lims = [
+    min(ax.get_xlim()[0], ax.get_ylim()[0]),
+    max(ax.get_xlim()[1], ax.get_ylim()[1])
+]
+ax.set_xlim(lims)
+ax.set_ylim(lims)
+ax.set_aspect('equal', adjustable='box')
+plt.legend(title='Grandinės', bbox_to_anchor=(1.05, 0.25), loc='lower left')
+plt.title(f'MDS Projekcija. Stress: {mds_low.stress_:.4f}, max_iter=500, n_init=16, init=classical_mds')
 plt.xlabel('MDS1')
 plt.ylabel('MDS2')
-plt.axis('equal')
 plt.show()
+
+
+
+# %%
+
 
 
