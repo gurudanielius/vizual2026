eaf97226152cac3a6f4809a22b4c024a7ed23f5f
Emilija Abromaitytė <emilija.abromaityte@mif.stud.vu.lt>
2026-04-08 22:40:49 +0300

    prideti sezonai

--- lab3.py
+++ lab3.py
@@ -1,9 +1,11 @@
 # %%
 import pandas as pd
 
+
 # %%
 data_raw = pd.read_csv('Elektrines_duomenys_2023-2024m.csv', sep=';', decimal=',')
 data_raw
+
 
 # %%
 #sum across rows
@@ -13,11 +15,14 @@ data_final["timestamp"] = pd.to_datetime(data_final["timestamp"], errors="coerce
 data_final = data_final.dropna(subset=["timestamp"])
 
 
+
 # %%
 data_final.isna().sum()
 
+
 # %%
 data_final
+
 
 # %%
 # Convert data_final to wide format: Day + one column per 5-minute timestamp
@@ -36,7 +41,22 @@ sum_of_inv_wide = sum_of_inv_wide.reset_index()
 final_dataset = sum_of_inv_wide[["Day"] + sorted(sum_of_inv_wide.columns[1:])]
 final_dataset
 
+
 # %%
 final_dataset.to_csv("final_dataset.csv", index=False)
+
+# %%
+final_dataset["Day"] = pd.to_datetime(final_dataset["Day"], errors="coerce").dt.date
+final_dataset["month"] = pd.to_datetime(final_dataset["Day"]).dt.month
+
+# %%
+season_map = {
+    12: "Winter", 1: "Winter", 2: "Winter",
+    3: "Spring", 4: "Spring", 5: "Spring",
+    6: "Summer", 7: "Summer", 8: "Summer",
+    9: "Autumn", 10: "Autumn", 11: "Autumn"
+}
+
+final_dataset["season"] = final_dataset["month"].map(season_map)
 
 
